edeb332ffa6e40fcf330987a23fcc0d2caa3faeb
ryuhanda <r.handa@everyleaf.com>
2022-06-02 00:32:07 +0900

    不要なコメントを削除

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,27 +1,22 @@
 class CatsController < ApplicationController
   before_action :set_cat, only: %i[ show edit update destroy ]
 
-  # GET /cats
   def index
    @search = Cat.ransack(params[:q])
    @search.sorts = 'id desc' if @search.sorts.empty?
    @cats = @search.result.page(params[:page])
   end
 
-  # GET /cats/1
   def show
   end
 
-  # GET /cats/new
   def new
     @cat = Cat.new
   end
 
-  # GET /cats/1/edit
   def edit
   end
 
-  # POST /cats
   def create
     @cat = Cat.new(cat_params)
 
@@ -32,7 +27,6 @@ class CatsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
       redirect_to @cat, notice: "ねこを更新しました。"
@@ -41,7 +35,6 @@ class CatsController < ApplicationController
     end
   end
 
-  # DELETE /cats/1
   def destroy
     @cat.destroy
     redirect_to cats_url, notice: "ねこを削除しました"
